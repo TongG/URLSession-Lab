fb0d80f218cb115b4cd2df56e8f31426ee740e11
Tong Guo <Tong-G@outlook.com>
2015-03-19 19:07:00 +0800

    Backup

--- URLSession Lab/USLAppDelegate.m
+++ URLSession Lab/USLAppDelegate.m
@@ -10,9 +10,34 @@
 
 @implementation USLAppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
-}
+- ( void ) applicationDidFinishLaunching: ( NSNotification* )_Notification
+    {
+    NSURLSession* defaultSession =
+        [ NSURLSession sessionWithConfiguration: [ NSURLSessionConfiguration defaultSessionConfiguration ] ];
+
+    NSURL* hotTopicsAPI = [ NSURL URLWithString: @"https://www.v2ex.com/api/topics/hot.json" ];
+    NSURLSessionDataTask* hotTopics = [ defaultSession dataTaskWithURL: hotTopicsAPI
+                                                     completionHandler:
+        ^( NSData* _Data, NSURLResponse* _Response, NSError* _Error )
+            {
+            NSError* error = nil;
+
+            NSMutableArray* responseBody =
+                [ NSJSONSerialization JSONObjectWithData: _Data options: NSJSONReadingMutableLeaves | NSJSONReadingAllowFragments error: &error ];
+
+            if ( !error )
+                NSLog( @"Hot Topics: %@", responseBody );
+            else
+                [ self.window presentError: error ];
+
+            NSLog( @"Response: %@",_Response );
+
+            if ( _Error )
+                NSLog( @"Error: %@", _Error );
+            } ];
+
+    [ hotTopics resume ];
+    NSLog( @"Count of bytes expected to receive: %lld", hotTopics.countOfBytesExpectedToReceive );
+    }
 
 @end
--- URLSession LabTests/URLSession_LabTests.m
+++ URLSession LabTests/URLSession_LabTests.m
@@ -7,6 +7,9 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <AppKit/NSApplication.h>
+
+#import "USLAppDelegate.h"
 
 @interface URLSession_LabTests : XCTestCase
 
@@ -26,9 +29,68 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
+- ( void ) testDefaultSession
+    {
+    NSURLSession* defaultSession =
+        [ NSURLSession sessionWithConfiguration: [ NSURLSessionConfiguration defaultSessionConfiguration ] ];
+
+    NSURL* hotTopicsAPI = [ NSURL URLWithString: @"https://www.v2ex.com/api/topics/hot.json" ];
+    NSURLSessionDataTask* hotTopicsTask = [ defaultSession dataTaskWithURL: hotTopicsAPI
+                                                         completionHandler:
+        ^( NSData* _Data, NSURLResponse* _Response, NSError* _Error )
+            {
+            NSError* error = nil;
+
+            NSMutableArray* responseBody =
+                [ NSJSONSerialization JSONObjectWithData: _Data options: NSJSONReadingMutableLeaves | NSJSONReadingAllowFragments error: &error ];
+
+            if ( !error )
+                NSLog( @"Hot Topics: %@", responseBody );
+
+            NSLog( @"Response: %@",_Response );
+            NSLog( @"Error: %@", _Error );
+            } ];
+
+    NSURLSession* sessionCopy0 = [ defaultSession copy ];
+    NSURLSession* sessionCopy1 = [ defaultSession copy ];
+    NSURLSession* sessionCopy2 = [ defaultSession copy ];
+    NSURLSession* sessionCopy3 = [ defaultSession copy ];
+
+    XCTAssertEqual( defaultSession, sessionCopy0 );
+    XCTAssertEqual( sessionCopy1, sessionCopy2 );
+    XCTAssertEqual( sessionCopy2, sessionCopy3 );
+    XCTAssertEqual( sessionCopy3, defaultSession );
+
+    NSURLSessionDataTask* hotTopicsTaskCopy0 = [ hotTopicsTask copy ];
+    NSURLSessionDataTask* hotTopicsTaskCopy1 = [ hotTopicsTask copy ];
+    NSURLSessionDataTask* hotTopicsTaskCopy2 = [ hotTopicsTask copy ];
+    NSURLSessionDataTask* hotTopicsTaskCopy3 = [ hotTopicsTask copy ];
+
+    XCTAssertEqual( hotTopicsTask, hotTopicsTaskCopy0 );
+    XCTAssertEqual( hotTopicsTaskCopy0, hotTopicsTaskCopy1 );
+    XCTAssertEqual( hotTopicsTaskCopy2, hotTopicsTaskCopy3 );
+    XCTAssertEqual( hotTopicsTaskCopy3, hotTopicsTask );
+
+    NSURLSessionConfiguration* configurationCopy0 = [ defaultSession configuration ];
+    NSURLSessionConfiguration* configurationCopy1 = [ defaultSession configuration ];
+    NSURLSessionConfiguration* configurationCopy2 = [ defaultSession configuration ];
+    NSURLSessionConfiguration* configurationCopy3 = [ defaultSession configuration ];
+
+    XCTAssertNotEqual( configurationCopy0, configurationCopy1 );
+    XCTAssertNotEqual( configurationCopy1, configurationCopy2 );
+    XCTAssertNotEqual( configurationCopy2, configurationCopy3 );
+    XCTAssertNotEqual( configurationCopy3, configurationCopy0 );
+
+    [ hotTopicsTask resume ];
+    }
+
+- ( void ) testBackgroundTransfer
+    {
+    NSURLSession* downloadSession = [ NSURLSession sessionWithConfiguration:
+        [ NSURLSessionConfiguration backgroundSessionConfiguration: @"individual.TongGuo.URLSesson-Lab" ] ];
+
+    NSURLSession* copy1 = [ downloadSession copy ];
+    NSURLSession* copy2 = [ downloadSession copy ];
+    }
 
 @end
